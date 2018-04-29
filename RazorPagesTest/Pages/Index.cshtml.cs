0cf48d723ec54b5bf111a54c7719011a4d30a52b
Kimserey <kim.lam@tigerspike.com>
2018-04-29 09:43:23 +0800

    Added subfolder

---
+++ RazorPagesTest/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void Test()
+        {
+            
+        }
+    }
+}
---
+++ RazorPagesTest/Pages/Tests/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesTest.Pages.Tests
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
